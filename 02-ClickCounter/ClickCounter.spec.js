a2c7a1e94a78e17cacc39709acb906333363373b
Jongkeun Kim <kjkandrea@gmail.com>
2020-10-03 19:22:46 +0900

    test : red

--- 02-ClickCounter/ClickCounter.spec.js
+++ 02-ClickCounter/ClickCounter.spec.js
@@ -1,7 +1,8 @@
-describe('ClickCounter', ()=> {
+describe('App.ClickCounter', ()=> {
   describe('getCounter()', ()=> {
     it('초기값이 0인 카운터 값을 반환한다', ()=> {
-      // todo 
+      const counter = App.ClickCounter()
+      expect(counter.getValue()).toBe(0) // getValue의 값이 0이라는 것을 검증
     })
   })
 })
