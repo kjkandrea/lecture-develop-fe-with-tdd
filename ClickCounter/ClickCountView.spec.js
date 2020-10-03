68ad6a5cee8a173b533d42809c742d87eb56062f
Jongkeun Kim <kjkandrea@gmail.com>
2020-10-03 20:28:00 +0900

    test : red

--- ClickCounter/ClickCountView.spec.js
+++ ClickCounter/ClickCountView.spec.js
@@ -1,7 +1,17 @@
 describe('App.ClickCountView', ()=> {
+  let clickCounter, updateEl, view
+
+  beforeEach(()=> {
+    clickCounter = App.ClickCounter()
+    updateEl = document.createElement('span')
+    view = App.ClickCountView(clickCounter, updateEl)
+  })
+
   describe('updateView()', ()=> {
     it('ClickCounter의 getValue() 값을 출력한다', ()=> {
-      // todo 
+      const counterValue = clickCounter.getValue()
+      view.updateView()
+      expect(updateEl.innerHTML).toBe(counterValue.toString())
     })
   })
 })
