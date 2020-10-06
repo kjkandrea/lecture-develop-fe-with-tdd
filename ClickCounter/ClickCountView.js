ed48ebd137d534a97cdd5fd38c77bea10b568a15
kimjongkeun <jongkeun@godo.co.kr>
2020-10-06 20:49:39 +0900

    test : green

--- ClickCounter/ClickCountView.js
+++ ClickCounter/ClickCountView.js
@@ -8,6 +8,9 @@ App.ClickCountView = (clickCounter, updateEl) => {
     updateView() {
       updateEl.innerHTML = clickCounter.getValue()
     },
+    increaseAndUpdateView() {
+      clickCounter.increase()
+    }
   }
 }
 
